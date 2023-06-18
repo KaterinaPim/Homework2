444a0f92b6f5dbf9d67c234142025d7426902224
Katrin <halyukka@gmail.com>
2023-06-18 19:51:04 +0700

    exercise 23

---
+++ exercise23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console. Clear();
+Console.Write("Введите число N: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Таблица кубов:\n");
+for (int i=1;i<=a;i++)
+{
+    int b = i*i*i;
+    Console.Write($"{b}, ");
+}
